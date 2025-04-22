eb33d3dc9ca59b46b53612a1b8428506ca37147a
Saiful Islam Auny <aunychowdhury99@gmail.com>
2025-04-23 00:58:47 +0600

    new deploy

--- myportfolio_backend/settings.py
+++ myportfolio_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://my-portfolio-xmok.onrender.com','https://*.127.0.0.1']
 
